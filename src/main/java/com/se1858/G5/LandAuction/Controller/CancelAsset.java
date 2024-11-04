9b67320e80519efe8f986eeb183e9216692081a1
Tung Pham <tungpvhe186583@fpt.edu.vn>
2024-11-04 17:05:27 +0700

    Update

---
+++ src/main/java/com/se1858/G5/LandAuction/Controller/CancelAsset.java
@@ -0,0 +1,29 @@
+package com.se1858.G5.LandAuction.Controller;
+
+import com.se1858.G5.LandAuction.Service.AssetRegistrationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("cancel")
+public class CancelAsset {
+    private AssetRegistrationService assetRegistrationService;
+
+    public CancelAsset(AssetRegistrationService assetRegistrationService) {
+        this.assetRegistrationService = assetRegistrationService;
+    }
+
+    @GetMapping()
+    public String cancel() {
+
+        return "/customer/cancel-form";
+    }
+
+    @PostMapping()
+    public String cancelAsset() {
+
+        return "/customer/cancel-form";
+    }
+}
