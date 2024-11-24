80c6c876198374bebc5070f143b0893dcf806d8c
Tung Pham <tungpvhe186583@fpt.edu.vn>
2024-11-25 00:01:33 +0700

    Update

--- src/main/java/com/se1858/G5/LandAuction/Controller/UserController.java
+++ src/main/java/com/se1858/G5/LandAuction/Controller/UserController.java
@@ -297,6 +297,7 @@ public class UserController {
             assetRegistrationService.save(assetRegistration);
             return "redirect:/asset";
         }
+        assetRegistration.setReason(cancelAssetDTO.getReason());
         Violation violation = new Violation();
         violation.setUser(user);
         Auction auction = auctionService.findAuctionByLand(assetRegistration.getLand());
